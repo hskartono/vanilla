340998cd431cbb5425a2c168c4da3d68cc480301
Salman <salman31@rocketmail.com>
2016-08-04 11:45:25 +0700

    [ADD]
    - add variable path in gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -5,31 +5,38 @@ var rename = require('gulp-rename');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
+const ALL_JS_PATH = 'amn/js/*.js';
+const ALL_CSS_PATH = 'amn/css/*.less';
+const DEST_JS = 'amn/js/';
+const DEST_CSS = 'amn/css/';
+const SRC_JS = 'amn/js/';
+const SRC_CSS = 'amn/css/';
+
 gulp.task('default', function(){
     console.log('okeh jalan');
 });
 
 gulp.task('less-minify-css', function(){
-    gulp.src('amn/css/master.less')
+    gulp.src(SRC_CSS + 'master.less')
         .pipe(less())
         .pipe(clean_css({
             keepSpecialComments:0,
             processImport:0
         }))
         .pipe(rename('master.min.css'))
-        .pipe(gulp.dest('amn/css/'));
+        .pipe(gulp.dest(DEST_CSS));
 });
 
 gulp.task('minify-js', function(){
-    gulp.src(['amn/js/amn_component.js', 'amn/js/amn_popup.js'])
+    gulp.src([SRC_JS + 'amn_component.js', SRC_JS + 'amn_popup.js'])
         .pipe(concat('amn_master.js'))
-        .pipe(gulp.dest('amn/js/'))
+        .pipe(gulp.dest(DEST_JS))
         .pipe(rename('amn_master.min.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('amn/js/'));
+        .pipe(gulp.dest(DEST_JS));
 });
 
 gulp.task('watch', function(){
-    gulp.watch('amn/js/*.js',['minify-js']);
-    gulp.watch('amn/css/*.less',['less-minify-css']);
+    gulp.watch(ALL_JS_PATH, ['minify-js']);
+    gulp.watch(ALL_CSS_PATH, ['less-minify-css']);
 });
